cdac0c8b9904143adcbe551a626a345b35f71214
devonlin <devonlin101@gmail.com>
2021-04-06 14:50:12 -0400

    remove rotate 360 deg page transition

--- script.js
+++ script.js
@@ -51,3 +51,29 @@ barba.init({
   }]
 });
 
+        function menuToggle() {
+          var nav = document.getElementById("menu-overlay");
+          nav.classList.toggle("active");
+          var nav = document.getElementById("toggleIcon");
+          nav.classList.toggle("active");
+        }
+        const counter = document.querySelector(".counter");
+        console.log(counter);
+        const speed = 200;
+
+        const updateCount = () => {
+          const target = +counter.getAttribute("data-target");
+          const count = +counter.innerText;
+          const inc = target / speed;
+          if (count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 10);
+          } else {
+            count.innerText = target;
+          }
+        };
+        updateCount();
+        function cb(response) {
+          counter.innerText = response.value;
+        }
+
