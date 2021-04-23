b3a0376e125d476e4e8c8a53520d39caed0634f9
devonlin <devonlin101@gmail.com>
2021-04-23 18:47:40 -0400

    fixed mobile version ui layout bug

--- script.js
+++ script.js
@@ -1,79 +1,50 @@
-// do not import Barba like this if you load the library through the browser
-import barba from '@barba/core';
+function menuToggle() {
+  var nav = document.getElementById("menu-overlay");
+  nav.classList.toggle("active");
+  var nav = document.getElementById("toggleIcon");
+  nav.classList.toggle("active");
+}
+const counter = document.querySelector(".counter");
+console.log(counter);
+const speed = 200;
 
-// basic default transition (with no rules and minimal hooks)
-barba.init({
-  transitions: [{
-    leave({ current, next, trigger }) {
-      // do something with `current.container` for your leave transition
-      // then return a promise or use `this.async()`
-    },
-    enter({ current, next, trigger }) {
-      // do something with `next.container` for your enter transition
-      // then return a promise or use `this.async()`
-    }
-  }]
-});
+const updateCount = () => {
+  const target = +counter.getAttribute("data-target");
+  const count = +counter.innerText;
+  const inc = target / speed;
+  if (count < target) {
+    counter.innerText = Math.ceil(count + inc);
+    setTimeout(updateCount, 10);
+  } else {
+    count.innerText = target;
+  }
+};
+updateCount();
+function cb(response) {
+  counter.innerText = response.value;
+}
 
-// dummy example to illustrate rules and hooks
-barba.init({
-  transitions: [{
-    name: 'dummy-transition',
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
 
-    // apply only when leaving `[data-barba-namespace="home"]`
-    from: 'home',
+  if (h == 0) {
+    h = 12;
+  }
 
-    // apply only when transitioning to `[data-barba-namespace="products | contact"]`
-    to: {
-      namespace: [
-        'products',
-        'contact'
-      ]
-    },
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
 
-    // apply only if clicked link contains `.cta`
-    custom: ({ current, next, trigger })
-      => trigger.classList && trigger.classList.contains('cta'),
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
-    // do leave and enter concurrently
-    sync: true,
-
-    // available hooks…
-    beforeOnce() {},
-    once() {},
-    afterOnce() {},
-    beforeLeave() {},
-    leave() {},
-    afterLeave() {},
-    beforeEnter() {},
-    enter() {},
-    afterEnter() {}
-  }]
-});
-
-        function menuToggle() {
-          var nav = document.getElementById("menu-overlay");
-          nav.classList.toggle("active");
-          var nav = document.getElementById("toggleIcon");
-          nav.classList.toggle("active");
-        }
-        const counter = document.querySelector(".counter");
-        console.log(counter);
-        const speed = 200;
-
-        const updateCount = () => {
-          const target = +counter.getAttribute("data-target");
-          const count = +counter.innerText;
-          const inc = target / speed;
-          if (count < target) {
-            counter.innerText = Math.ceil(count + inc);
-            setTimeout(updateCount, 10);
-          } else {
-            count.innerText = target;
-          }
-        };
-        updateCount();
-        function cb(response) {
-          counter.innerText = response.value;
-        }
-
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.querySelector(".clock").innerHTML = time;
+  setTimeout(showTime, 1000);
+}
